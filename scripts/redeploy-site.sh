8d392ecbf91f15ae9f5eaa310ca727b40e4c574e
Anand Tandon <anandtandon8@gmail.com>
2025-07-13 16:15:48 +0000

    changed redeploy script to restart the service instead of using tmux

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd /root/portfolio-site
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux_cmd="
-cd /root/portfolio-site
-source python3-virtualenv/bin/activate
-flask run --host=0.0.0.0 --port 80
-"
-tmux new-session -d -s portfolio-site "$tmux_cmd"
+sudo systemctl daemon-reload
+sudo systemctl restart myportfolio
 
