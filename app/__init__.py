dd2fe39d5436363d76365115700265c4acc73508
m3di0cre3 <anandtandon8@gmail.com>
2025-07-24 00:12:40 -0400

    text change to test new redeploy script with docker compose on vps

--- app/__init__.py
+++ app/__init__.py
@@ -87,7 +87,7 @@ def index():
     ]
 
     about_me = {
-        'description': 'I\'m currently studying at the University of Waterloo, with a major in Software Engineering. I just wrapped up my first year and am an aspiring software engineer. I also do photography on the side for fun.'
+        'description': 'I\'m currently studying at the University of Waterloo, doing a major in Software Engineering. I just wrapped up my first year and am an aspiring software engineer. I also do photography on the side for fun.'
     }
 
     travel_map = {
